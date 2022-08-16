ac90ef895e5cdf012deef4ac6007c86511b334d9
Gustavo do Amaral Bordoni <gustavo.bordoni@relux.local>
2022-08-16 09:38:36 -0300

    v1.0 done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const burgerBtn = document.getElementById('burger-button');
 const sideMenu = document.getElementById('navlinks');
+const blackOver = document.getElementById('black-overlay');
 
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('change')
     navlinks.classList.toggle('show')
+    blackOver.classList.toggle('curtain')
 })
